f2492e2f441e2d65fedc224bf768f8d0e7b23070
ggdaltoso <ggdaltoso@gmail.com>
2019-01-11 16:12:35 -0200

    feat(BTV-805): [WM] Add Frame decorator and semantic-ui

--- .storybook/config.js
+++ .storybook/config.js
@@ -1,9 +1,12 @@
-import { configure } from '@storybook/react';
+import { configure, addDecorator } from '@storybook/react';
+import Frame from '../src/decorators/Frame';
 
 const stories = require.context('../src/stories', false, /.story.jsx?$/);
 
 function loadStories() {
   stories.keys().map(stories);
 }
+
+addDecorator(Frame);
 
 configure(loadStories, module);
