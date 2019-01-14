8a2e2dc97b2959ae1fe679f4155ab9a6543ace7a
allyssonsantos <aallysson0@gmail.com>
2019-01-14 15:58:07 -0200

    Change color sctructure

--- src/ui/Colors.js
+++ src/ui/Colors.js
@@ -1,6 +1,13 @@
 const Colors = {
-  grey: ['#eceff1', '#d1d8dc', '#7b909c', '#526977'],
-  blue: ['#03a9f4'],
+  grey: {
+    light: '#eceff1',
+    medium: '#d1d8dc',
+    harder: '#7b909c',
+    strong: '#526977',
+  },
+  blue: {
+    light: '#03a9f4',
+  },
   black: '#000000',
   white: '#FFFFFF',
 };
