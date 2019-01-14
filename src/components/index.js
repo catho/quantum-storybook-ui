834aee93fd7da634a0c65b1973f5109fd3d93dbb
allyssonsantos <aallysson0@gmail.com>
2019-01-14 16:34:48 -0200

    feat(BTV-805): [WM] AutoPropsApi

--- src/components/index.js
+++ src/components/index.js
@@ -1,6 +1,6 @@
 // export { default as AutoExample } from './AutoExample';
 // export { default as AutoProps } from './AutoProps';
-// export { default as AutoPropsApi } from './AutoPropsApi';
+export { default as AutoPropsApi } from './AutoPropsApi';
 export { default as CodeExample } from './CodeExample';
 export { default as CodeToClipboard } from './CodeToClipboard';
 export { default as ComponentPanel } from './ComponentPanel';
