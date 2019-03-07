353995a8d9e40e40536c1cdfd512e145fcdd838a
Allysson Santos <aallysson0@gmail.com>
2019-03-07 16:37:12 -0300

    docs(BTS-85-storybook-looks): Remove title styles

--- src/components/GlobalStyle/sub-components/Typography.jsx
+++ src/components/GlobalStyle/sub-components/Typography.jsx
@@ -3,29 +3,12 @@ import Colors from '../../../ui/Colors';
 
 const Typography = createGlobalStyle`
   @import url('https://fonts.googleapis.com/css?family=Fira+Mono');
-  @import url('https://fonts.googleapis.com/css?family=Titillium+Web:400,400i,600,600i,700,700i');
+  @import url('https://fonts.googleapis.com/css?family=Nunito+Sans:400,600,600i,700');
 
   body {
-    font-family: 'Titillium Web';
+    font-family: 'Nunito Sans', sans-serif;
     color: ${Colors.blue['bayoux']};
     font-size: 17px;
-  }
-
-  h1, h2, h3, h4 {
-    color: ${Colors.blue['sanjuan']};
-    letter-spacing: -1px;
-  }
-
-  h1 {
-    font-size: 40px;
-  }
-
-  h2 {
-    font-size: 30px;
-  }
-
-  h3 {
-    font-size: 24px;
   }
 
   pre {
@@ -33,9 +16,11 @@ const Typography = createGlobalStyle`
   }
 
   code {
+    background-color: #f6f8fa;
+    display: inline-block;
     font-family: 'Fira Mono';
     font-size: 12px;
-    background-color: #f6f8fa;
+    line-height: 2.5;
     padding: 2px 5px;
   }
 `;
