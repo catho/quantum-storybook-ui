4963de3ed86ddc722d9202bbf7d4360299a9685d
Allysson Santos <aallysson0@gmail.com>
2019-03-07 16:38:06 -0300

    docs(BTS-85-storybook-looks): Add title styles

--- src/components/Title/Title.jsx
+++ src/components/Title/Title.jsx
@@ -8,11 +8,16 @@ const StyledTitle = styled.h1`
   color: ${Colors.blue['sanjuan']};
   display: flex;
   flex-direction: row;
+  font-size: 40px;
   font-weight: 700;
+  letter-spacing: -1px;
   line-height: 60px;
   margin: 0 0 30px 0;
   padding: 90px 0 0 0;
   text-align: left;
+
+  ${({ as }) => as === 'h2' && `font-size: 30px;`}
+  ${({ as }) => as === 'h3' && `font-size: 24px;`}
 `;
 
 const Title = ({ children, ...rest }) => (
