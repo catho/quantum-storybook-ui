e41935931a5e1bfde04bc84a574f5c45b578617d
Daniel Silva <daniel5055@gmail.com>
2019-06-14 09:52:02 -0300

    fix(AutoProps): Remove unnecessary variable

--- stories/Button/Button.jsx
+++ stories/Button/Button.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import styled from 'styled-components';
-import { shadow, hexToRgba } from '../shared';
+import { shadow } from '../shared';
 import {
   components,
   spacing,
